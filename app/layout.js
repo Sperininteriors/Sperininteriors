c3e36db66f42b0e17712e39018bdb17edd9bc041
Sperininteriors <kevin@sperininteriors.co.uk>
2026-04-20 22:16:46 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,11 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
   title: "Sperin Interiors",
   description: "Complete Home Renovation & Building Specialists",
 };
 
-import "./globals.css";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
